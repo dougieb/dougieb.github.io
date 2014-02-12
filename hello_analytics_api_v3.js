f6aaffab09653aa43e6c870a8092501d14d3e127
Doug <douglas.berger@gmail.com>
2014-02-12 17:07:27 -0600

    asdf

--- hello_analytics_api_v3.js
+++ hello_analytics_api_v3.js
@@ -129,7 +129,7 @@ function printResults(results) {
 	   	tr.append('<tr><th>OS / Browser</th><th>Visits</th><th>% of '+row.category+'</th></tr>');
 		jQuery.each(row.os, function(osName, osVisits) {
 			if ((osVisits / row.total) > .01){
-				tr.append('<tr><td>'+osName+'</td><td>'+osVisits+'</td><td>'+Math.round((osVisits / row.total)*100)+'%</td></tr>');
+				tr.append('<tr><td>'+osName+'</td><td>'+osVisits+'</td><td>'+((osVisits / row.total)*100).toFixed(1)+'%</td></tr>');
 			}
 		});
 		
